9097dbe2d74c34968036f2badbe1a8252f92627e
Matt <mwbertolucci@gmail.com>
2023-04-17 19:39:06 -0400

    Try to fix bug again

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import hallOfFameReducer from './hof/hallOfFameSlice';
+import hallOfFameSlice from './hof/hallOfFameSlice';
 
 const rootReducer = combineReducers({
-  hallOfFame: hallOfFameReducer,
+  hallOfFame: hallOfFameSlice,
 });
 
 const store = configureStore({
