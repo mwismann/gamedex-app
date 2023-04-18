d3981f81033d1a162609c7f5a49aeda7d81f371d
Matt <mwbertolucci@gmail.com>
2023-04-18 17:07:55 -0400

    Add temp fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import hallOfFameSlice from './hof/hallOfFameSlice';
+import hallOfFameSlice from './hof/temp';
 
 const store = configureStore({
   reducer: {
