58a6a791b50fca8ce3fb3263fc8f6517da25fad7
Matt <mwbertolucci@gmail.com>
2023-04-18 16:44:40 -0400

    Add upcomingSlice and actions

--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -1,5 +1,33 @@
-const Upcoming = () => (
-  <div>Upcoming</div>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Game from '../components/Game';
+import getUpcoming from '../redux/upcoming/actions';
+
+const Upcoming = () => {
+  const { upcoming, loading, error } = useSelector((state) => state.upcoming);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUpcoming());
+  }, []);
+
+  return (
+    <div>
+      <h1>Upcoming Games</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <ul>
+        {upcoming.map((game) => (
+          <li key={game.id}>
+            <Game
+              name={game.name}
+              img={game.images.banner.og}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Upcoming;
