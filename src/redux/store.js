7d9ab0943f1fb20485b31cb0869deb0033628bea
Matt <mwbertolucci@gmail.com>
2023-04-18 17:19:45 -0400

    Rename hallOfFameSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import hallOfFameSlice from './hof/temp';
+import hallOfFameSlice from './hof/hallOfFameSlice';
 import justReleasedSlice from './just_released/justReleasedSlice';
 import popularSlice from './popular/popularSlice';
 import upcomingSlice from './upcoming/upcomingSlice';
