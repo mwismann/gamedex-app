d2a3899c2147e4a59bf0c2d63080dcc75cc3e49f
Matt <mwbertolucci@gmail.com>
2023-04-19 18:30:24 -0400

    Add custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       fontFamily: {
         lato: ['Lato', 'sans-serif'],
       },
+      colors: {
+        main: '#3c6cb4',
+        secondary: '#1a4482',
+        black: 'rgba(0, 0, 0, 0.95)',
+      },
     },
   },
   plugins: [],
