76f63ca9b42c4494ce85d8e1232ff9f5e7c8d256
Matt <mwbertolucci@gmail.com>
2023-04-19 13:19:22 -0400

    Add Searchbar to filter pages' content

--- src/pages/HallOfFame.jsx
+++ src/pages/HallOfFame.jsx
@@ -1,23 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import getHallOfFame from '../redux/hof/actions';
 import Game from '../components/Game';
+import Searchbar from '../components/Searchbar';
 
 const HallOfFame = () => {
   const { hallOfFame, loading, error } = useSelector((state) => state.hallOfFame);
+  const [value, setValue] = useState('');
   const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
   useEffect(() => {
     dispatch(getHallOfFame());
   }, []);
 
+  const query = hallOfFame.filter(
+    (game) => game.name.toLowerCase().includes(value.toLowerCase()),
+  );
+
   return (
     <div>
       <h1>Hall of Fame</h1>
+      <Searchbar value={value} handleChange={handleChange} />
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       <ul>
-        {hallOfFame.map((game) => (
+        {query.map((game) => (
           <li key={game.id}>
             <Game
               page="hof"
--- src/pages/JustReleased.jsx
+++ src/pages/JustReleased.jsx
@@ -1,23 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import getJustReleased from '../redux/just_released/actions';
 import Game from '../components/Game';
+import Searchbar from '../components/Searchbar';
 
 const JustReleased = () => {
   const { justReleased, loading, error } = useSelector((state) => state.justReleased);
+  const [value, setValue] = useState('');
   const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
   useEffect(() => {
     dispatch(getJustReleased());
   }, []);
 
+  const query = justReleased.filter(
+    (game) => game.name.toLowerCase().includes(value.toLowerCase()),
+  );
+
   return (
     <div>
       <h1>Just Released</h1>
+      <Searchbar value={value} handleChange={handleChange} />
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       <ul>
-        {justReleased.map((game) => (
+        {query.map((game) => (
           <li key={game.id}>
             <Game
               page="just_released"
